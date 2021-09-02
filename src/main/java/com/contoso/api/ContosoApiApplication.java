28e5e96af0e6403fe030eb27e3b2f2a9c35b11f5
yspbiz85 <84260705+yspbiz85@users.noreply.github.com>
2021-09-02 11:59:32 +0530

    Initial commit
    Initial commit

---
+++ src/main/java/com/contoso/api/ContosoApiApplication.java
@@ -0,0 +1,13 @@
+package com.contoso.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContosoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContosoApiApplication.class, args);
+	}
+
+}
