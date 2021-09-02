a3350b691b2dbbc5ff950e8edeb1161b58a06549
yspbiz85 <84260705+yspbiz85@users.noreply.github.com>
2021-09-02 14:43:12 +0530

    Swagger Integration
    Swagger Integration

---
+++ src/main/java/com/contoso/api/configurations/OpenApiConfig.java
@@ -0,0 +1,21 @@
+package com.contoso.api.configurations;
+
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Contoso Gaming Platform API")
+                        .version("1.0")
+                        .description("This is a demo REST API project developed using Spring Boot,springdoc-openapi and OpenAPI 3.")
+                        .termsOfService("http://swagger.io/terms/")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")));
+    }
+}
