0c9b94c02a49532ee41394288d9fa778309b933c
yspbiz85 <84260705+yspbiz85@users.noreply.github.com>
2021-09-08 23:34:28 +0530

    Bug Fix

--- src/main/java/com/contoso/api/controllers/GamingController.java
+++ src/main/java/com/contoso/api/controllers/GamingController.java
@@ -1,20 +1,13 @@
 package com.contoso.api.controllers;
 
-import com.contoso.api.model.GraphSearch;
 import com.contoso.api.model.MasterGraphSearch;
 import com.contoso.api.model.RouteMaster;
 import com.contoso.api.services.GamingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.ArrayList;
+import org.springframework.web.bind.annotation.*;
 import java.util.List;
-import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1/gaming")
@@ -28,7 +21,7 @@ public class GamingController {
         return new ResponseEntity<>(this.gamingService.findDefaultGraphData(), HttpStatus.OK);
     }
 
-    @GetMapping("/calculateData")
+    @PostMapping("/calculateData")
     public ResponseEntity<List<RouteMaster>> findGraph(@RequestBody MasterGraphSearch masterGraphSearch) {
         return new ResponseEntity<>(this.gamingService.findGraphData(masterGraphSearch), HttpStatus.OK);
     }
